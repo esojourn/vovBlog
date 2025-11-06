f5817c9a0d9d64fe57c4537091f026ed81aa58ad
Ding Xuan <esojourn@gmail.com>
2025-11-06 21:25:41 +0800

    add favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,30 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: '瓦器 WaQi.uk',
   description: '充满瑕疵的脆弱器皿',
+  icons: {
+    icon: [
+      {
+        url: '/images/favicon.ico',
+      },
+      {
+        url: '/images/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: '/images/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+    ],
+    apple: '/images/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'manifest',
+        url: '/images/site.webmanifest',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
